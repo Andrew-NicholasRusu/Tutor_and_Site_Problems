bd29ad0aa751dff57e677d6efbf8470dfe8e98d1
Andrew-Nicholas Rusu <andrewnicholasrusu@gmail.com>
2026-04-28 13:14:11 -0400

    Implement recursive sum function for numbers 1 to N

--- Tutor_Work_&_Practices/Piyush_Chakerwarty_Java/April/April_27_2026/Recursion_Homework.java
+++ Tutor_Work_&_Practices/Piyush_Chakerwarty_Java/April/April_27_2026/Recursion_Homework.java
@@ -3,18 +3,18 @@
 
 import java.util.Scanner;
 public class Recursion_Homework {
-    Scanner sc = new Scanner(System.in);
-
-    public static int factorial(int n) {
-        if (n <= 1) {
+    public static int sum(int N) { // Recursion example:
+        if (N <= 1) {
             return 1;
         }
-        return n * factorial(n - 1);
+        return N + sum (N - 1); // Calculates the sum of N, which is the value of the user.
+        // N - 1 
     }
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Take N as an input fron the user:");
+        int N = sc.nextInt();
+        System.out.println("Sum of all numbers = " + sum(N));
     }
-    
-
 }
